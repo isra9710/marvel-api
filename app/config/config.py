be89182c0ef874cc300b4834b53d1175984a297c
isra9710 <isra.rios.con@gmail.com>
2022-07-24 23:36:21 -0500

    search comics

---
+++ app/config/config.py
@@ -0,0 +1,7 @@
+#Esta url nos sirve para buscar por id del comic
+url_search_comic_id = "https://gateway.marvel.com:443/v1/public/comics/"
+#Según se necesite, esta url nos sirve para buscar comics de la A-Z, por titulo especifico o
+#para un comic que "empice con"
+url_search_comic_title = "https://gateway.marvel.com:443/v1/public/comics?"
+#Lo requiere para consumirse
+ts = 1
